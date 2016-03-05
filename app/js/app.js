dfd36a7b89b281ab63dedf7c5b529311eb41125b
littleapis <bill_5945@163.com>
2016-03-05 10:13:28 +0800

    map

--- app/js/app.js
+++ app/js/app.js
@@ -1,26 +1,31 @@
-var mainApp = angular.module('mainApp',[]);
+var mainApp = angular.module('mainApp',['ui.router']);
 
-mainApp.controller('UserInfoCtrl',['$scope',function($scope){
-	$scope.userInfo={
-		email:'12312312@qq.com',
-		password:123456,
-		autoLogin:true
-	}
-	$scope.getFormData = function(){
-		console.log($scope.userInfo);
-	}
-	$scope.setFormData=function(){
-		$scope.userInfo={
-			email:'760788722@qq.com',
-			password:"damoqiongqiu",
-			autoLogin:false
-		}
-	}
-	$scope.resetFormData=function(){
-		$scope.userInfo={
-			email:'',
-			password:"",
-			autoLogin:false
-		}
-	}
-}])
+
+
+angular.module('bmap',[])
+    .factory('bmapService', ['$document', '$q', '$rootScope',
+        function($document, $q, $rootScope) {
+            var map = $q.defer();
+            function onScriptLoad() {
+                // Load client in the browser
+                $rootScope.$apply(function() { map.resolve(window.bmap); });
+            }
+            // Create a script tag with d3 as the source
+            // and call our onScriptLoad callback when it
+            // has been loaded
+            var scriptTag = $document[0].createElement('script');
+            scriptTag.type = 'text/javascript';
+            scriptTag.async = true;
+            scriptTag.src = 'http://api.map.baidu.com/api?type=quick&ak=XXXX&v=1.0';
+            scriptTag.onreadystatechange = function () {
+                if (this.readyState == 'complete') onScriptLoad();
+            };
+            scriptTag.onload = onScriptLoad;
+
+            var s = $document[0].getElementsByTagName('body')[0];
+            s.appendChild(scriptTag);
+
+            return {
+                bmap: function() { return map.promise; }
+            };
+        }]);
